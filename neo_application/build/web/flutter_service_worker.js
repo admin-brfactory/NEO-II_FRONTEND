0c447c69dda19fd38646f6d81834b32c95418215
LucasSalinoBRF <lucas.salino@brfactory.com.br>
2022-04-29 11:12:48 -0300

    Ajustes na tela de login

--- neo_application/build/web/flutter_service_worker.js
+++ neo_application/build/web/flutter_service_worker.js
@@ -23,9 +23,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "a13175fb877ab2095e94d86d15fe56ec",
-"/": "a13175fb877ab2095e94d86d15fe56ec",
-"main.dart.js": "d7807c85e49ae432e04b19bcdc3715ea",
+"index.html": "bcd2481666e9493664e749f5087cff79",
+"/": "bcd2481666e9493664e749f5087cff79",
+"main.dart.js": "880e5074ecd1a866ae7b7b20c95bf843",
 "manifest.json": "689d85e07f16f38de3a561956def330b",
 "version.json": "31ad7459046c8ebc10c17e09127cccad"
 };
